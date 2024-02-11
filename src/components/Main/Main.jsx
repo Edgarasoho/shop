fee042e0bac0f0353bd6a701a00b9876687edb5c
Edgarasoho <edgaras.osenis@gmail.com>
2024-02-11 20:09:08 +0200

    shop pakeitimai

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { mockData } from "../../mockData";
+import "./Main.scss";
+import Card from "../Cards/Card";
 
 function Main() {
+  console.log(mockData);
   return (
-    <main>
-      <div>
-        <h1>Item</h1>
-      </div>
+    <main className="main-container">
+      {mockData.map((item) => (
+        <Card
+          key={item.title}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
     </main>
   );
 }
