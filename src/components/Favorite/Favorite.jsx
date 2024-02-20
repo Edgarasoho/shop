0157e8a2abba6492892d8c3dfce162e2096c9336
Edgarasoho <edgaras.osenis@gmail.com>
2024-02-20 23:01:32 +0200

    userLoading

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -3,30 +3,30 @@ import React, { useContext } from "react";
 import Card from "../Cards/Card";
 import SortButton from "../SortButton/SortButon";
 import { handleSort } from "../../utils/sortUtils";
-import { AppContext } from "../../context/AppConte";
+import { AppContext } from "../../context/AppContext";
 
 function Favorite() {
-  const { favoriteDate, setFavoriteData } = useContext(AppContext);
+  const { favoriteData, setFavoriteData } = useContext(AppContext);
 
   const handleSortData = (direction) => {
-    const sortedData = handleSort(favoriteDate, direction);
+    const sortedData = handleSort(favoriteData, direction);
     setFavoriteData(sortedData);
   };
+
+  return (
+    <main className="main-container">
+      <SortButton handleSortData={handleSortData} />
+
+      {favoriteData.map((item) => (
+        <Card
+          key={item.title}
+          title={item.title}
+          description={item.description}
+          handleCardButton={() => {}}
+        />
+      ))}
+    </main>
+  );
 }
-
-return (
-  <main className="main-container">
-    <SortButton handleSortData={handleSortData} />
-
-    {favoriteDate.map((item) => (
-      <Card
-        key={item.title}
-        title={item.title}
-        description={item.description}
-        handleCardButton={() => {}}
-      />
-    ))}
-  </main>
-);
 
 export default Favorite;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.scss";
 
 function Navbar() {
+  const [user, setUser] = useState("user");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users/2"
+        );
+
+        if (!response.ok) throw new Error("Something went wrong");
+        const data = await response.json();
+
+        console.log(data);
+
+        setUser(data.username);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <nav className="nav-container">
       <h1>My shop</h1>
+
       <ul>
         <li>
           <NavLink to="/"> All items</NavLink>
@@ -17,6 +43,7 @@ function Navbar() {
           <NavLink to="/Favorite"> Favorite</NavLink>{" "}
         </li>
       </ul>
+      {loading ? <h1 className="loading">Loading</h1> : <h1>{user}</h1>}
     </nav>
   );
 }
